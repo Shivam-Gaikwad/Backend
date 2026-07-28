6cb43f472d3b33ebdb12fc087528efe875bcca2a
Shivam Gaikwad <gaikwadshivam572@gmail.com>
2026-07-28 08:59:36 +0530

    Backend started

---
+++ index.js
@@ -0,0 +1,26 @@
+import http, { Server } from "http";
+const port = 8000;
+const server = http.createServer((req,res) => {
+    if(req.url == "/"){
+        res.end("welcome to home page: ")
+    }else if(req.url == "/about"){
+        res.end("This is an about page")
+    }else if(req.url == "/contactUs"){
+        res.end(`
+            {
+            "name" : "Shivam",
+            "Education" : "B.E.(IT)",
+            "status" : "unemployed",
+           "contact Number" : "8669061300",
+            "emailID" : "gaikwadshivam572@gmail.com"
+            }
+            `)
+    }
+    else{
+        res.end("Welcome to MERN Stackk")
+    }
+})
+
+server.listen(8000,()=>{
+console.log("Server is started");
+})
