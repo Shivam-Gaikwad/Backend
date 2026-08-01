4e34cba941a76658803b0418b8b2ef6e8637582e
user.name "Shivam Gaikwad <user.email "gaikwadshivam572@gmail.com>
2026-08-01 22:24:52 +0530

    final changes

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ app.get("/",(req,res) => {
 app.listen(port, () => {
     console.log(`server is started at ${port}`)
 })
+
+//  //NODE.JS SERVER CREATION
+
+// import http from "http";
+// const port = 8000;
+// const server = http.createServer((req,res) => {
+//     if(req.url == "/"){
+//         res.end("welcome to the mern stack course: ")
+//     }else if(req.url == "/about"){
+//         res.end("WE provide an professional IT and Software solutions")
+//     }else{
+//         res.end("Error 404: Page not found")
+//     }
+// })
+
+// server.listen(port, ()=>{
+//     console.log("server started")
+// })
